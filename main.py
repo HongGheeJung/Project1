94986e5834ba0eb796f19e6a6958b314fc92ca6e
Hong Ghee Jung <rlwjd258@gmail.com>
2024-07-02 16:14:46 +0900

    제곱커밋

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ def sub_func(n1, n2):
     retVal = n1 - n2
     return retVal
 
+def multwo_func(n1, n2):
+    retVal = n1 ** n2
+    return retVal
+
 ## 변수부
 
-num1, num2, res = 100, 200, 0
+num1, num2, res = 10, 2, 0
 
 
 ## 메인 코드부
@@ -20,3 +24,6 @@ print(num1, '+', num2, '=', res)
 
 res = sub_func(num1, num2)
 print(num1, '-', num2,'=', res)
+
+res = multwo_func(num1, num2)
+print(num1, '**', num2,'=', res)
