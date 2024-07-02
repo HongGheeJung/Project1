439fa9cd47cdb47051dea23e7d121dc163788b4f
Hong Ghee Jung <rlwjd258@gmail.com>
2024-07-02 14:30:51 +0900

    두번쨰 커밋
    빼기함수 구현

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ def add_func(n1, n2):
     retVal = n1 + n2
     return retVal
 
+def sub_func(n1, n2):
+    retVal = n1 - n2
+    return retVal
+
 ## 변수부
 
 num1, num2, res = 100, 200, 0
@@ -12,3 +16,7 @@ num1, num2, res = 100, 200, 0
 
 res = add_func(num1, num2)
 print(num1, '+', num2, '=', res)
+
+
+res = sub_func(num1, num2)
+print(num1, '-', num2,'=', res)
