ac0e52688d94f7f4249828423b61c0c73b91bad0
Hong Ghee Jung <rlwjd258@gmail.com>
2024-07-02 15:33:51 +0900

    Revert "세번째 커밋"
    This reverts commit e9c9604e4ba16f69b7271aab04000e3294de1c9f.

--- main.py
+++ main.py
@@ -7,14 +7,6 @@ def sub_func(n1, n2):
     retVal = n1 - n2
     return retVal
 
-def mul_func(n1, n2):
-    retVal = n1 * n2
-    return retVal
-
-def dev_func(n1, n2):
-    retVal = n1 / n2
-    return retVal
-
 ## 변수부
 
 num1, num2, res = 100, 200, 0
@@ -28,10 +20,3 @@ print(num1, '+', num2, '=', res)
 
 res = sub_func(num1, num2)
 print(num1, '-', num2,'=', res)
-
-res = mul_func(num1, num2)
-print(num1, '*', num2, '=', res)
-
-
-res = dev_func(num1, num2)
-print(num1, '/', num2,'=', res)
